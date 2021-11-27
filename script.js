4224a981e4467cc9c11c6b795af8ce7faae69542
Real Inspiration <ilerwatson22@gmail.com>
2021-11-27 12:09:16 +0000

    getItem has now been added to local storage so any textInput set in local storage will be shown in html

--- script.js
+++ script.js
@@ -1,30 +1,48 @@
 $(document).ready(function () {
 
+
+    $("#currentDay").text(moment().format('dddd, Do, MMM'));
+
+
     $(".saveBtn").on("click", function () {
         var textInput = $(this).siblings(".inputText").val()
         var time = $(this).parent().attr("id")
         console.log(textInput)
-        // console.log(time)
-        localStorage.setItem(time, textInput);
+        console.log(time)
+        localStorage.setItem(time, textInput)
     })
-    
-    
-    $(".inputText").val(localStorage.getItem(textInput))
 
 
+    $("#time9 .inputText").val(localStorage.getItem("time9"));
+    $("#time10 .inputText").val(localStorage.getItem("time10"));
+    $("#time11 .inputText").val(localStorage.getItem("time11"));
+    $("#time12 .inputText").val(localStorage.getItem("time12"));
+    $("#time1 .inputText").val(localStorage.getItem("time1"));
+    $("#time2 .inputText").val(localStorage.getItem("time2"));
+    $("#time3 .inputText").val(localStorage.getItem("time3"));
+    $("#time4 .inputText").val(localStorage.getItem("time4"));
+    $("#time5 .inputText").val(localStorage.getItem("time5"));
 
 
-    var currentTime = moment().hour()
+    function timeUpdate() {
 
-    if(currentTime > 10){
-        $('.time10').addClass('past')
-    } else if(currentTime === 10){
-        
-        $('.time10').addClass('present')
-    }else {
-        $('.time10').addClass('future')
+        var currentTime = moment.duration().hours();
+
+        $("#hourBlock").each(function () {
+            var blockHour = parseInt($(this).attr('id').split("-")[1]);
+
+            if (blockHour < currentTime) {
+                $(this).addClass("past");
+            } else if (blockHour === currentTime) {
+                $(this).removeClass("past");
+                $(this).addClass("present");
+            } else {
+                $(this).removeClass("past");
+                $(this).removeClass("present");
+                $(this).addClass("future");
+            }
+        });
     }
 
-
-    $("#currentDay").text(moment().format('dddd, Do, MMM'));
+    timeUpdate()
 })
