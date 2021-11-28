d47a1192c745c658aa3d9d8be737934b50959366
Real Inspiration <ilerwatson22@gmail.com>
2021-11-28 18:08:35 +0000

    if statement added to change time block colour depending on time

--- script.js
+++ script.js
@@ -13,6 +13,90 @@ $(document).ready(function () {
     })
 
 
+    var currentTime = moment().hour();
+
+    if (currentTime > 9) {
+        $('.time9').addClass('past')
+    }
+    else if (currentTime === 9) {
+        $('.time9').addClass('present')
+    }
+    else {
+        $('.time9').addClass('future')
+    }
+    if (currentTime > 10) {
+        $('.time10').addClass('past')
+    }
+    else if (currentTime === 10) {
+        $('.time10').addClass('current')
+    }
+    else {
+        $('.time10').addClass('future')
+    }
+    if (currentTime > 11) {
+        $('.time11').addClass('past')
+    }
+    else if (currentTime === 11) {
+        $('.time11').addClass('present')
+    }
+    else {
+        $('.time11').addClass('future')
+    }
+    if (currentTime > 12) {
+        $('.time12').addClass('past')
+    }
+    else if (currentTime === 12) {
+        $('.time12').addClass('present')
+    }
+    else {
+        $('.time12').addClass('future')
+    }
+    if (currentTime > 1) {
+        $('.time1').addClass('past')
+    }
+    else if (currentTime === 1) {
+        $('.time1').addClass('present')
+    }
+    else {
+        $('.time1').addClass('future')
+    }
+    if (currentTime > 2) {
+        $('.time2').addClass('past')
+    }
+    else if (currentTime === 2) {
+        $('.time2').addClass('present')
+    }
+    else {
+        $('.time2').addClass('future')
+    }
+    if (currentTime > 3) {
+        $('.time3').addClass('past')
+    }
+    else if (currentTime === 3) {
+        $('.time3').addClass('present')
+    }
+    else {
+        $('.time3').addClass('future')
+    }
+    if (currentTime > 4) {
+        $('.time4').addClass('past')
+    }
+    else if (currentTime === 4) {
+        $('.time4').addClass('present')
+    }
+    else {
+        $('.time4').addClass('future')
+    }
+    if (currentTime > 5) {
+        $('.time5').addClass('past')
+    }
+    else if (currentTime === 5) {
+        $('.time5').addClass('present')
+    }
+    else {
+        $('.time5').addClass('future')
+    }
+
     $("#time9 .inputText").val(localStorage.getItem("time9"));
     $("#time10 .inputText").val(localStorage.getItem("time10"));
     $("#time11 .inputText").val(localStorage.getItem("time11"));
@@ -22,27 +106,8 @@ $(document).ready(function () {
     $("#time3 .inputText").val(localStorage.getItem("time3"));
     $("#time4 .inputText").val(localStorage.getItem("time4"));
     $("#time5 .inputText").val(localStorage.getItem("time5"));
-
-
-    function timeUpdate() {
-
-        var currentTime = moment.duration().hours();
-
-        $("#hourBlock").each(function () {
-            var blockHour = parseInt($(this).attr('id').split("-")[1]);
-
-            if (blockHour < currentTime) {
-                $(this).addClass("past");
-            } else if (blockHour === currentTime) {
-                $(this).removeClass("past");
-                $(this).addClass("present");
-            } else {
-                $(this).removeClass("past");
-                $(this).removeClass("present");
-                $(this).addClass("future");
-            }
-        });
-    }
-
-    timeUpdate()
 })
+    
+
+
+
